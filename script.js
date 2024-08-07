3d57e6d03ac1df4ec41c0f229c7d65c9e0cb4771
emre g. <emreodev1998@gmail.com>
2024-08-07 17:26:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const space=document.querySelector(".space");
 const mouseTracker=new mouseEffect.mouseTracker(parallaxMain,parallax);
 mouseTracker.percentageScroll=40;//right and left max scroll
 let mouseEvent=window.addEventListener("pointermove", mouseTracker.getMouseCallback());
-window.ondragstart=()=>false;
+document.documentElement.ondragstart=()=>false;
 
 
 
